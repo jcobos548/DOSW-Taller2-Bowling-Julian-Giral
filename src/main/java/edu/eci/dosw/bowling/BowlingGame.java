879627d2dad457f3a377e5e31397ca2324f085dd
Julian Camilo Giral Cobos <julian.giral-c@mail.escuelaing.edu.co>
2026-09-17 21:48:21 -0500

    feat: GREEN - valida rango de pinos

--- src/main/java/edu/eci/dosw/bowling/BowlingGame.java
+++ src/main/java/edu/eci/dosw/bowling/BowlingGame.java
@@ -14,8 +14,8 @@ public class BowlingGame {
     }
 
     public void roll(int pins) {
-        if (pins < 0) {
-            throw new IllegalArgumentException("Los pinos no pueden ser negativos");
+        if (pins < 0 || pins > 10) {
+            throw new IllegalArgumentException("Los pinos deben estar entre 0 y 10");
         }
 
         Frame frame = new Frame();
