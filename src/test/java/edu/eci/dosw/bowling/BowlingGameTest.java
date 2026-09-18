fbcd2679050a51c7a11d279f9abb35d204a04b15
Julian Camilo Giral Cobos <julian.giral-c@mail.escuelaing.edu.co>
2026-09-17 21:44:02 -0500

    test: RED - roll rechaza pinos negativos

--- src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
+++ src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
@@ -2,8 +2,7 @@ package edu.eci.dosw.bowling;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingGameTest {
 
@@ -14,4 +13,15 @@ class BowlingGameTest {
         assertDoesNotThrow(() -> game.roll(0));
         assertEquals(0, game.getFrames().get(0).getRolls().get(0));
     }
+
+    @Test
+    void rollNegativePins_throwsException() {
+        BowlingGame game = new BowlingGame();
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> game.roll(-1)
+        );
+    }
 }
+
