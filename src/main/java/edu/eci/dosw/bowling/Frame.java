7b56aacacfddbfcb55c8f3c36520b04f38b78206
Julian Camilo Giral Cobos <julian.giral-c@mail.escuelaing.edu.co>
2026-09-17 22:00:37 -0500

    feat: GREEN - registra strike

--- src/main/java/edu/eci/dosw/bowling/Frame.java
+++ src/main/java/edu/eci/dosw/bowling/Frame.java
@@ -6,16 +6,26 @@ import java.util.List;
 public class Frame {
 
     private final List<Integer> rolls;
+    private FrameType type;
 
     public Frame() {
         this.rolls = new ArrayList<>();
+        this.type = FrameType.NORMAL;
     }
 
     public void addRoll(int pins) {
         rolls.add(pins);
+
+        if (pins == 10 && rolls.size() == 1) {
+            type = FrameType.STRIKE;
+        }
     }
 
     public List<Integer> getRolls() {
         return List.copyOf(rolls);
+    }
+
+    public FrameType getType() {
+        return type;
     }
 }
