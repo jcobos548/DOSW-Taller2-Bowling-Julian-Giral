c1c6e6a707ed1da0e82587c74f9122094bdec713
Julian Camilo Giral Cobos <julian.giral-c@mail.escuelaing.edu.co>
2026-09-17 21:45:33 -0500

    feat: GREEN - valida pinos negativos

--- src/main/java/edu/eci/dosw/bowling/BowlingGame.java
+++ src/main/java/edu/eci/dosw/bowling/BowlingGame.java
@@ -14,6 +14,10 @@ public class BowlingGame {
     }
 
     public void roll(int pins) {
+        if (pins < 0) {
+            throw new IllegalArgumentException("Los pinos no pueden ser negativos");
+        }
+
         Frame frame = new Frame();
         frame.addRoll(pins);
         frames.add(frame);
